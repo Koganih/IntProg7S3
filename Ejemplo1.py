5b3e9547cdfb2c9929fa4f072674f58a7808254d
Koganih <gizamora@uamv.edu.ni>
2025-03-31 12:28:24 -0600

    Trabajo 1 semana 3

---
+++ Ejemplo1.py
@@ -0,0 +1,10 @@
+"""
+calcular la nota de un estudiante, que
+tiene 2 sistemaricos de 30
+y uno de 40
+"""
+sistematico1 = int(input("ingrese primer sistematico (max: 30): "))
+sistematico2 = int(input("ingrese segundo sistematico (max: 30): "))
+sistematico3 = int(input("ingrese tercer sistematico (max: 30): "))
+nota = (sistematico1 + sistematico2 + sistematico3)
+print(("la nota del estudiante es: "), nota)
